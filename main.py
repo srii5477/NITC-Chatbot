96e81a2874f115587dc4bd336264567aca105ca5
Sride Shankar <sridevishankar1161@gmail.com>
2025-01-04 02:31:45 +0530

    added dept info files

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ options = Options()
 options.headless = False  
 options.add_argument("--window-size=1920,1200")
 
-URL = "https://nitc.ac.in/news-and-events"  
+# URL = "https://nitc.ac.in/news-and-events"  
+URL = "https://nitc.ac.in/department/electronics-amp-communication-engineering"
 
 DRIVER_PATH = "C:\\Users\\sride\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"
 
@@ -40,18 +41,37 @@ driver.get(URL)
 #         writer.writerow(["Title", "Date"]) 
 #         writer.writerows(data)  
 
-links = driver.find_elements(By.CLASS_NAME, 'xc-footer-links')
-departments = []
-for i in links:
+# links = driver.find_elements(By.CLASS_NAME, 'xc-footer-links')
+# departments = []
+# for i in links:
+#     print(i.text)
+#     departments.append(i.text)
+
+# departments = [item.replace(",", "") for item in departments]
+# departments = [item.replace("\"", "") for item in departments]
+# with open("depts.csv", "w", newline="", encoding="utf-8") as file:
+#         writer = csv.writer(file)
+#         writer.writerow(["Title"])  
+#         for item in departments:
+#             writer.writerow([item])
+# cse = driver.find_elements(By.XPATH, "//div[@class='xc-department-content']//p")
+# cse_content = []
+# for i in cse:
+#     print(i.text)
+#     cse_content.append(i.text)
+# with open("cse.csv", "w", newline="", encoding="utf-8") as file:
+#     writer = csv.writer(file)
+#     writer.writerow(["Description of the CSE Department"])  
+#     for item in cse_content:
+#         writer.writerow([item])
+ece = driver.find_elements(By.XPATH, "//div[@class='xc-department-content']//p")
+ece_content = []
+for i in ece:
     print(i.text)
-    departments.append(i.text)
-
-departments = [item.replace(",", "") for item in departments]
-departments = [item.replace("\"", "") for item in departments]
-with open("depts.csv", "w", newline="", encoding="utf-8") as file:
-        writer = csv.writer(file)
-        writer.writerow(["Title"])  
-        for item in departments:
-            writer.writerow([item])
-
+    ece_content.append(i.text)
+with open("ece.csv", "w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Description of the ECE Department"])  
+    for item in ece_content:
+        writer.writerow([item])
 driver.quit()
